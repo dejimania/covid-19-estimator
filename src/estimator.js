72a3466d2344280c5568e17a5bc4dc15f5151423
Kamil Balogun <kamilbalogun1@gmail.com>
2020-04-08 21:34:05 +0100

    Fixed estimator issues

--- src/estimator.js
+++ src/estimator.js
@@ -18,19 +18,19 @@ const normalizeDays = (types) => {
 // Infection estimator
 const estimator = (infection, pTypes, period) => {
   let estimatedInfection;
-  let day = parseInt(period, 10);
+  let factor;
+  let day = period;
   if (day === 1 || day === 2) {
     if (pTypes === 'months' || pTypes === 'weeks') {
-      day = normalizeDays(pTypes) * period;
-      const factor = 2 ** Math.trunc(day / 3);
+      day *= normalizeDays(pTypes);
+      factor = 2 ** Math.trunc(day / 3);
       estimatedInfection = infection * factor;
     } else {
       estimatedInfection = infection * day;
     }
   } else {
-    day = normalizeDays(pTypes) * period;
-    // const factor = Math.pow(2, Math.trunc(day / 3));
-    const factor = 2 ** Math.trunc(day / 3);
+    day *= normalizeDays(pTypes);
+    factor = 2 ** Math.trunc(day / 3);
     estimatedInfection = infection * factor;
   }
   return estimatedInfection;
@@ -41,7 +41,6 @@ const covid19ImpactEstimator = (data) => {
   const input = data;
   const impact = {};
   const severeImpact = {};
-  // if(input.reportedCases) {
   impact.currentlyInfected = input.reportedCases * 10;
   severeImpact.currentlyInfected = input.reportedCases * 50;
 
@@ -50,7 +49,6 @@ const covid19ImpactEstimator = (data) => {
   severeImpact.infectionsByRequestedTime = estimator(severeImpact.currentlyInfected,
     input.periodType, input.timeToElapse);
 
-  // }
   return {
     input,
     impact,

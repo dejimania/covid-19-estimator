f0a3f45fb8e36c5ef0cf68b51a140f514580df70
Kamil Balogun <kamilbalogun1@gmail.com>
2020-04-09 17:03:21 +0100

    Completed challenges 2 and 3

--- src/estimator.js
+++ src/estimator.js
@@ -16,7 +16,7 @@ const normalizeDays = (types) => {
 };
 
 // Infection estimator
-const estimator = (infection, pTypes, period) => {
+const estimateInfected = (infection, pTypes, period) => {
   let estimatedInfection;
   let factor;
   let day = period;
@@ -37,6 +37,25 @@ const estimator = (infection, pTypes, period) => {
   return estimatedInfection;
 };
 
+// Challenge 2: total number of available bed spaces
+const estimateAvailableBeds = (totalHospBeds, severeCasesReqByTime) => {
+  const bedAvailable = 0.35 * totalHospBeds;
+  const hospBedsByReqTime = bedAvailable - severeCasesReqByTime;
+  return Math.trunc(hospBedsByReqTime);
+};
+
+//  challenge 3: Loss to economy
+const estimateLossToEconomy = (infectedByReqTime, data) => {
+  const {
+    region: { avgDailyIncomeInUSD: income, avgDailyIncomePopulation: population },
+    periodType,
+    timeToElapse: time
+  } = data;
+  const period = normalizeDays(periodType) * time;
+  const dollarsInFlight = (infectedByReqTime * income) * period * population;
+  return dollarsInFlight;
+};
+
 
 const covid19ImpactEstimator = (data) => {
   const input = data;
@@ -45,10 +64,45 @@ const covid19ImpactEstimator = (data) => {
   impact.currentlyInfected = input.reportedCases * 10;
   severeImpact.currentlyInfected = input.reportedCases * 50;
 
-  impact.infectionsByRequestedTime = estimator(impact.currentlyInfected,
+  impact.infectionsByRequestedTime = estimateInfected(impact.currentlyInfected,
     input.periodType, input.timeToElapse);
-  severeImpact.infectionsByRequestedTime = estimator(severeImpact.currentlyInfected,
+  severeImpact.infectionsByRequestedTime = estimateInfected(severeImpact.currentlyInfected,
     input.periodType, input.timeToElapse);
+  // challenge 2
+  impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
+  severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
+  // estimate Beds space available
+  impact.hospitalBedsByRequestedTime = estimateAvailableBeds(
+    input.totalHospitalBeds, impact.severeCasesByRequestedTime
+  );
+  severeImpact.hospitalBedsByRequestedTime = estimateAvailableBeds(
+    input.totalHospitalBeds, severeImpact.severeCasesByRequestedTime
+  );
+
+  // challenge 3
+  // ICU
+  impact.casesForICUByRequestedTime = Math.trunc(
+    0.05 * impact.infectionsByRequestedTime
+  );
+  severeImpact.casesForICUByRequestedTime = Math.trunc(
+    0.05 * severeImpact.infectionsByRequestedTime
+  );
+
+  // Ventilator
+  impact.casesForVentilatorsByRequestedTime = Math.trunc(
+    0.02 * impact.infectionsByRequestedTime
+  );
+  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(
+    0.02 * severeImpact.infectionsByRequestedTime
+  );
+
+  // Lose to economy
+  impact.dollarsInFlight = estimateLossToEconomy(
+    impact.infectionsByRequestedTime, input
+  );
+  severeImpact.dollarsInFlight = estimateLossToEconomy(
+    severeImpact.infectionsByRequestedTime, input
+  );
 
   return {
     input,
